489426bfad2ea870c3fab7f6bf0b9e9a7fbefb72
guildc <cameliaguild@sbcglobal.net>
2016-05-01 19:32:16 -0500

    add plot1.R

---
+++ plot1.R
@@ -0,0 +1,44 @@
+# Load in useful libraries
+library(dplyr)
+library (data.table)
+
+#set working directory
+setwd("/Users/cameliaguild/Projects/DataScientistsToolbox/Exploratory-Data-Analysis/Plotting1")
+
+filename <- "exdata_dataset.zip"
+
+# Download and unzip data files:
+if (!file.exists(filename)) {
+  fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileURL, filename, method="curl")
+  unzip(filename)
+}
+
+temp <- read.table("./household_power_consumption.txt", header=TRUE, na.strings="?", stringsAsFactors = FALSE, sep=";")
+temp$Date <- as.Date(temp$Date, format = "%d/%m/%Y")
+pc <- temp[(temp$Date =="2007-02-01") | (temp$Date=="2007-02-02"),]
+
+names(pc)
+pc$Global_active_power <- as.numeric(as.character(pc$Global_active_power))
+pc$Global_reactive_power <- as.numeric(as.character(pc$Global_reactive_power))
+pc$Voltage <- as.numeric(as.character(pc$Voltage))
+pc$Sub_metering_1 <- as.numeric(as.character(pc$Sub_metering_1))
+pc$Sub_metering_2 <- as.numeric(as.character(pc$Sub_metering_2))
+pc$Sub_metering_3 <- as.numeric(as.character(pc$Sub_metering_3))
+
+# Create a new column that combines date and time variables
+ pc$Timestamp <- paste(pc$Date, pc$Time)
+ #pc <- transform(pc, Timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S" )
+names(pc)
+
+# 
+plot1 <- function(){
+        hist(pc$Global_active_power, col = "red", main = "Global Active Power", 
+        xlab = "Global Active Power (kilowatts)")
+        dev.copy(png, file="plot1.png", width=480, height=480)
+        dev.off()
+        cat("Plot1.png has been saved in", getwd())
+}
+plot1()
+
+
